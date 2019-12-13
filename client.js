a7489a7f2295cdd15ccbc5912815d4eac7bb9797
Paige Wielgos <paigewielgos@Paiges-MacBook-Pro.local>
2019-12-13 16:36:41 -0600

    delete employee up, monthly cost appending variable to DOM

--- client.js
+++ client.js
@@ -2,15 +2,21 @@ console.log('js');
 
 $(document).ready(onReady);
 
+const totalMonthly = 0;
+let employees = [];
+
 function onReady() {
     console.log('in onReady');
-
     $('#submitBtn').on('click', addEmployee);
-
+    $('#employeeList').on('click', '.deleteBtn', deleteEmployee);
+    let el = $('#totalMonthly');
+    el.empty();
+    el.append(`Total Monthly: ${totalMonthly}`);
 }
 
 function addEmployee() {
     console.log('submit button clicked');
+    // get user inputs
     let firstName = $('#firstNameIn').val();
     let lastName = $('#lastNameIn').val();
     let idNumber = $('#idNumberIn').val();
@@ -20,12 +26,13 @@ function addEmployee() {
 
     // append user inputs to the DOM
     $('#employeeList').append(`<tr>
-        <td>${firstName}<td>
-        <td>${lastName}<td>
-        <td>${idNumber}<td>
-        <td>${title}<td>
-        <td>${annualSalary}<td>
-        <tr>`);
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${idNumber}</td>
+    <td>${title}</td>
+    <td>${annualSalary}</td>
+    <td><button class="deleteBtn">DELETE</button></td>
+    </tr>`);
 
     // empty values
     $('#firstNameIn').val('');
@@ -33,4 +40,18 @@ function addEmployee() {
     $('#idNumberIn').val('');
     $('#titleIn').val('');
     $('#annualSalaryIn').val('');
+
+    // push user inputs into an array so we can calculate monthly cost
+    let newEmployee = {
+        
+    }
+}
+
+function deleteEmployee(){
+    console.log('in deleteEmployee');
+    $(this).closest('tr').remove();
+}
+
+function calculateTotalSalary(){
+
 }
