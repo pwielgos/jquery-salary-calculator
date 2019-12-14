71ae8df0050948926b5087d40957983bb7b4fd51
Paige Wielgos <paigewielgos@Paiges-MacBook-Pro.local>
2019-12-13 19:59:24 -0600

    calculations working and appending

--- client.js
+++ client.js
@@ -2,19 +2,19 @@ console.log('js');
 
 $(document).ready(onReady);
 
-const totalMonthly = 0;
 let employees = [];
 
 function onReady() {
     console.log('in onReady');
     $('#submitBtn').on('click', addEmployee);
-    $('#employeeList').on('click', '.deleteBtn', deleteEmployee);
-    let el = $('#totalMonthly');
-    el.empty();
-    el.append(`Total Monthly: ${totalMonthly}`);
+    $('#employeeList').on('click', '.deleteBtn', deleteEmployee); 
+    
+    displayEmployees(employees);
+    calculateMonthlyCosts();
 }
 
-function addEmployee() {
+function addEmployee(event) {
+    event.preventDefault();
     console.log('submit button clicked');
     // get user inputs
     let firstName = $('#firstNameIn').val();
@@ -22,19 +22,8 @@ function addEmployee() {
     let idNumber = $('#idNumberIn').val();
     let title = $('#titleIn').val();
     let annualSalary = $('#annualSalaryIn').val();
-    console.log(firstName, lastName, idNumber, title, annualSalary);
 
-    // append user inputs to the DOM
-    $('#employeeList').append(`<tr>
-    <td>${firstName}</td>
-    <td>${lastName}</td>
-    <td>${idNumber}</td>
-    <td>${title}</td>
-    <td>${annualSalary}</td>
-    <td><button class="deleteBtn">DELETE</button></td>
-    </tr>`);
-
-    // empty values
+    // empty inputs
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
     $('#idNumberIn').val('');
@@ -42,16 +31,45 @@ function addEmployee() {
     $('#annualSalaryIn').val('');
 
     // push user inputs into an array so we can calculate monthly cost
-    let newEmployee = {
-        
-    }
+    let newEmployee = { firstName, lastName, idNumber, title, annualSalary };
+    employees.push(newEmployee);
+    
+    displayEmployees(employees);
+    calculateMonthlyCosts();
 }
 
-function deleteEmployee(){
+function calculateMonthlyCosts() {
+  
+    // calculate monthly cost = employee.annualSalary value / 12
+    let monthlyCost = 0;
+    for (let i = 0; i < employees.length; i++) {
+        monthlyCost += Number(employees[i].annualSalary / 12);
+        }
+
+    // append monthly cost to DOM
+    let el = $('#totalMonthly');
+    el.empty();
+    el.append(`Total Monthly: ${monthlyCost}`);
+}
+
+function deleteEmployee() {
     console.log('in deleteEmployee');
     $(this).closest('tr').remove();
 }
 
-function calculateTotalSalary(){
-
+function displayEmployees(employeeArray) {
+    // append user input values to the DOM
+    $('#employeeList').empty();
+    for(let employee of employeeArray){
+    $('#employeeList').append(`<tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.idNumber}</td>
+        <td>${employee.title}</td>
+        <td>${employee.annualSalary}</td>
+        <td><button class="deleteBtn">DELETE</button></td>
+        </tr>`);
+    }
 }
+
+
